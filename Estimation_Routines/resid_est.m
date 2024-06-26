6b7c98f11cb2bacaa4cf100fd9a047ae9a9604e1
rche_mmini <rogerchen6879@gmail.com>
2024-06-26 15:05:59 +1000

    changes to residual estimator

--- Estimation_Routines/resid_est.m
+++ Estimation_Routines/resid_est.m
@@ -1,4 +1,4 @@
-function [outputArg1,outputArg2] = resid_est(inputArg1,inputArg2)
+function [IRF, n_lags_est] = resid_est(data_sim,settings)
 %RESID_EST Summary of this function goes here
 %   Detailed explanation goes here
 outputArg1 = inputArg1;
