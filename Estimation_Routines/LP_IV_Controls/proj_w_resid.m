27f0d6deff0f05c661e51633acc5f4f90dddb7b9
rche_mmini <rogerchen6879@gmail.com>
2024-07-03 00:12:32 +1000

    Completed second version of LP-IV estimator with additional controls
    ran initial simulation results and sample data testing and see bugs
    
    requries changing LP_IV_controls_est to remove t+h for z and y1

---
+++ Estimation_Routines/LP_IV_Controls/proj_w_resid.m
@@ -0,0 +1,39 @@
+function resid = proj_w_resid(input_vec_position, Y, IV_location, nlags, h, include_proxy)
+% input_vec_position defines the column of Y (the data matrix) that is going to be projected on the control
+% include_proxy, 0 = only set the lagged y_{t-1}... y_{t-p} as control, 1 = include "future" z_t
+
+% To get the residuals, we need the input vector, control vector (for proj matrix)
+input_vector = Y(:, input_vec_position);
+T = size(input_vector, 1);
+y = Y(:, IV_location+1:end);
+z = Y(:, IV_location);
+
+% create lagged y's
+% lagged_y_col = zeros(T-h-nlags, 1+K*nlags+h)
+
+% for i:nlags
+lagged_y = lagmatrix(y, 1:nlags);
+
+if include_proxy == 1
+    if h==0
+        W = [ones(T-h-nlags,1), lagged_y(nlags+1:end-h, :)];
+    else 
+        lagged_z = lagmatrix(z, 0:h-1);
+        W = [ones(T-h-nlags,1), lagged_y(nlags+1:end-h, :), lagged_z(nlags+h+1:end, :)];
+    end
+else
+    W = [ones(T-h-nlags,1), lagged_y(nlags+1:end-h, :)];
+end
+% ^ column of lagged y's stacked as [y_{t-1}; y_{t-2}; ... y_{t-p}]
+input_vector = input_vector(nlags+1+h:end);
+
+resid = input_vector - W * inv(W' * W) * W' * input_vector;
+
+% figure;
+% hold on
+% plot(input_vector)
+% plot(resid)
+% legend('input vec', 'resid')
+
+end
+
--- Estimation_Routines/LP_IV_controls_est.m
+++ Estimation_Routines/LP_IV_controls_est.m
@@ -1,16 +1,25 @@
 function [IRF,n_lags_est] = LP_IV_controls_est(data_sim,settings)
-%LP_IV_CONTROLS_EST Summary of this function goes here
-
 % preparations
-
 run('Estimation_Setup'); % common setup for all estimation methods
 
-% note recursiveShock = 1 for this estimator since this is the definition
-% of IV and observed shock.
-
+% note recursiveShock = 1 for this estimator since this is the definition of IV and observed shock.
 % Y is a T x 6 vector with the IV in the first column
+% Y1 corresponds to the policy variable <-> i_t (Gov spend or Monetary policy)
 
-[IRF,w] = IRF_LP_IVC(Y,recursiveShock,responseV,nlags,IRF_hor - 1); % IRF to one unit of shock
+IRF = zeros(IRF_hor, 1);
+
+% loop over horizons:
+for h = 0:IRF_hor-1
+    % Step 1: Obtain the "projection residuals" Y_t^\perp and z_t^\perp (Stock and Watson, 2018)
+    yr_res = proj_w_resid(responseV, Y, recursiveShock, nlags, h, include_proxy);
+    z_res = proj_w_resid(recursiveShock, Y, recursiveShock, nlags, h, include_proxy);
+    y1_res = proj_w_resid(policyV, Y, recursiveShock, nlags, h, include_proxy); % this is the location of the policy variable
+    
+    % Step 2: Compute the estimator as the fraction of the two sums
+    num = sum(yr_res.*z_res);
+    denom = sum(y1_res.*z_res);
+    IRF(h+1) = num / denom;
+end
 
 end
 
