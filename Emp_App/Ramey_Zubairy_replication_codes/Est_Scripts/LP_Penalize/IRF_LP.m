27fc470afe615576058b903ed8561dc33a0efe71
Roger <rogerchen6879@gmail.com>
2024-07-16 16:03:23 +1000

    added BC estimator (point, no CIs) to Ramey

---
+++ Emp_App/Ramey_Zubairy_replication_codes/Est_Scripts/LP_Penalize/IRF_LP.m
@@ -0,0 +1,23 @@
+function [irf,w] = IRF_LP(y, x, w,recurShock,respV,nlags,nhorizons)
+% Auxiliary function for estimating IRFs using least-squares LP
+
+nT = size(y,1);
+
+% error checking
+if (nhorizons + nlags) >= nT
+    error("Number of horizons too large! No obs in sample!")
+end
+
+irf = zeros(1,nhorizons + 1);
+
+% go thru horizon 0 to horizon max
+for h = 0:nhorizons
+    [~,~,Bx,~,~,~,w1] = LP(y, x, w,recurShock,respV,nlags,h); % h-step ahead LP
+    irf(1,h + 1) = Bx;
+    if h==0
+        w=w1; % Store control data vector for later bias correction if desired
+    end
+end
+
+end
+
