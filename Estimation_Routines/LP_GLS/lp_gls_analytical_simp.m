e0c504101b83efd062ee5843c4709b76ebda728b
Roger Chen <rogerchen6879@gmail.com>
2024-10-10 00:18:37 +1100

    fixed analytical GLS estimator

--- Estimation_Routines/LP_GLS/lp_gls_analytical_simp.m
+++ Estimation_Routines/LP_GLS/lp_gls_analytical_simp.m
@@ -1,4 +1,4 @@
-function Beff = lp_gls_analytical_simp(y, arp, maxh) 
+function Beff = lp_gls_analytical_simp(y, arp, maxh, recurShock, responseV) 
 
 %Analytical LP GLS Code from Lusompa (2023), modified for point estimates only without bias adjustment
 
@@ -6,63 +6,42 @@ function Beff = lp_gls_analytical_simp(y, arp, maxh)
     %y is the qxT matrix of variables
     %arp is the lag length
     %maxh is the maximum desired LP horizon
+    %recurShock is the location of the shock in the data vector
+    %responseV is the location of the responseV in the data vector
 
 %Output:
-    %Beff is the qxqxmaxh matrix of the wold impulse responses. For
-    %example Beff(:,:,h)' gives the horizon h estimate of the wold impulse
+    %Beff is the (maxh+1)x1 vector of the specific shock-response pair IRFs
 
-Y = y; 
-[q, T] = size(Y); 
-  
-%population estimates for h=1
+[Q, T] = size(y);
+Beff = zeros(maxh+1,1);
+
+% Estimation for h=1 (instantaneous response, matches OLS LP)
 h = 1;
-saveY = Y; 
-  
-p = 1 + arp*q; 
-F = zeros(q*arp, T-arp-h+1); 
-for j = 1:arp 
-    F((j-1)*q+(1:q), :) = Y(:, (arp-j+1):(T-h+1-j));  
-end
-  
-yeff = saveY(:, arp+h:end)'; %Dependent variable
- 
-Teffective = size(yeff', 2);
-F = [ones(1, Teffective); F]; % Design Matrix
- 
-[betahat, ~, ~, epshat] = ols(yeff, F', 0);
+[Y, X, W] = LP_gen_data(y', recurShock, responseV, arp, h);  % Note: y is transposed here
+X_full = [ones(size(X,1),1), X, W];
+[Beta, ~, ~, ~] = LS(Y, X_full);
+Beff(h) = Beta(2);  % The impulse response coefficient
 
-Beff = zeros(q, q, maxh);
-Beff(:, :, h) = betahat(2:q+1, :);
-
-epshat = epshat - mean(epshat);
-
-%population estimate for h>=2 
-for h = 2:maxh 
-    epscorrect = zeros(T-arp-h+1, q);  
-          
-    %gls correction for population estimate
-    for hsub = 1:h-1 
-        epscorrect = epscorrect + epshat(h-hsub:T-arp-hsub, :) * Beff(:, :, hsub);
-    end 
-    
-    Y = y;  
-    saveY = Y; 
- 
-    yeff = saveY(:, arp+h:end)' - epscorrect; %Dependent Variable
-
-    p = 1 + arp*q; 
-    F = zeros(q*arp, T-arp-h+1); 
-    for j = 1:arp 
-        F((j-1)*q+(1:q), :) = Y(:, (arp-j+1):(T-h+1-j)); 
+% Estimation for h>=1
+for h = 2:maxh
+    F = zeros(T-arp-h+1, Q*arp);
+    y_temp = y';
+    for j = 1:arp
+        F(:,(j-1)*Q+(1:Q)) = y_temp((arp-j+1):(T-h+1-j), :);
     end
-  
-    Teffective = size(yeff', 2);
-    F = [ones(1, Teffective); F]; % Design Matrix
- 
-    %population estimate
-    [betahat, ~, ~, ~] = ols(yeff, F', 0);
     
-    Beff(:, :, h) = betahat(2:q+1, :);
+    yeff = y_temp((arp+h):end, responseV);
+    Teffective = size(yeff, 1);
+    F = [ones(Teffective, 1), F];
+    
+    epscorrect = zeros(T-arp-h+1, 1);
+    for hsub = 1:h-1
+        epscorrect = epscorrect + y_temp((arp+h-hsub):(T-hsub), recurShock) * Beff(hsub+1);
+    end
+    yeff = yeff - epscorrect;
+    
+    [betahat, ~, ~, ~] = LS(yeff, F);
+    Beff(h+1) = betahat(recurShock+1);
 end
 
-return
+end
--- Estimation_Routines/LP_GLS_est.m
+++ Estimation_Routines/LP_GLS_est.m
@@ -6,13 +6,14 @@ function [IRF,n_lags_est] = LP_GLS_est(data_sim,settings);
 run('Estimation_Setup'); % common setup for all estimation methods
 
 % estimate IRF via LP
-Beff = lp_gls_analytical_simp(Y', nlags, IRF_hor);
+Beff = lp_gls_analytical_simp(Y', nlags, IRF_hor, recursiveShock, responseV);
 
 % IRF = squeeze(Beff(responseV, responseV, 1:IRF_hor));
-IRF = squeeze(Beff(recursiveShock, responseV, 1:IRF_hor)); % Beff(i,j) gives response of jth variable since original observables are transposed
+IRF = Beff;
 
 % normalize?
-IRF_normalize_gls = Beff(recursiveShock,recursiveShock,1)
+Beff_normalise = lp_gls_analytical_simp(Y', nlags, 1, recursiveShock, recursiveShock);
+IRF_normalize_gls = Beff_normalise(1)
 % IRF = IRF/IRF_normalize_gls;
 
 IRF = IRF';
